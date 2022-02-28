c469420eedebb451a61e578dcc2f3102a8311d5a
ryanjbrown799 <ryanjbrown799@gmail.com>
2022-02-27 20:42:30 -0500

    esp8266 sends mac and ip to server, server post back to esp8266

--- led/led.ino
+++ led/led.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <mDNSResolver.h>
+#include <ESP8266WebServer.h>
 
 #define WIFI_SSID       ""
 #define WIFI_PASS       ""
@@ -9,17 +10,14 @@
 #define BLUEPIN 2
 #define NAME_TO_RESOLVE "DESKTOP-U2DK7RL.local"
 
+ESP8266WebServer server(80);  
 
 WiFiUDP udp;
 mDNSResolver::Resolver resolver(udp);
 IPAddress ip;
-HTTPClient http;
 
-void setup() {
-  
-  Serial.begin(115200);
-
-  //connect to wifi
+//connect to wifi
+void ConnectWifi(){
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   
@@ -33,16 +31,20 @@ void setup() {
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
-  
+}
+
+//use mDns to resolve ip address of local server
+void FindLocalServer(){
   //find ip address of local server
   Serial.print("Resolving ");
   Serial.println(NAME_TO_RESOLVE);
+  
   int res=0;
   while(res==0){
     //use mDns to find our local server
     resolver.setLocalIP(WiFi.localIP());
-  
     ip = resolver.search(NAME_TO_RESOLVE);
+    
     if(ip != INADDR_NONE) {
       Serial.print("Resolved: ");
       Serial.println(ip);
@@ -50,21 +52,47 @@ void setup() {
     } else {
       Serial.println("Not resolved");
     }
-  }
+  }     
+}
+
+//send http post to local server
+void sendPost(String route,String msg){
+  HTTPClient http;
   
-  //test connection to local server
-  String addr = String("http://"+ip.toString()+":3000/test");
+  String addr = String("http://"+ip.toString()+":3000"+route);
+  
   http.begin(addr);      //Specify request destination
-  http.addHeader("Content-Type", "text/plain");
- 
-  int httpCode = http.POST("Message from ESP8266");   //Send the request
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  Serial.println("msg: "+msg);
+  
+  int httpCode = http.POST(msg);   //Send the request
   String payload = http.getString();                  //Get the response payload
  
   Serial.println(httpCode);   //Print HTTP return code
   Serial.println(payload);    //Print request response payload
  
-  http.end();  //Close connection
+  http.end();  //Close connection    
+  
+}
 
+void handleLED() {                         
+  Serial.println(server.arg("plain"));         
+  server.send(200);                         
+}
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  ConnectWifi();
+  FindLocalServer();
+
+  //send mac and ip to server
+  String info = String("mac="+WiFi.macAddress()+"&"+"ip="+WiFi.localIP().toString());
+  sendPost("/test", info);
+  
+  server.on("/LED", HTTP_POST, handleLED);
+  server.begin(); 
   //set pins and set strip to blue
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
@@ -76,6 +104,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient();
 
 }
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express')
+var request = require('request');
+
 const app = express()
 const port = 3000
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
 
 app.post('/test', (req, res) => {
+	console.log(req.body)
 	res.send("recieved")
+	
+	//send post back using ip recieved 
+	request.post({
+		headers: {'content-type' : 'application/x-www-form-urlencoded'},
+		url:     "http://"+req.body.ip+"/LED",
+		body:    "heydude"
+	}, function(error, response, body){
+		console.log(body);
+	});
+		
 })
 
 app.get('/', (req, res) => {
@@ -15,4 +31,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log("listening on port " +port)
 })
+
 
